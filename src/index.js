805e8d26f18b5aee7d24bc7b41bfb1c0d7203bf3
sospedra <sospedra.r@gmail.com>
2018-02-07 10:40:22 +0100

    feat: Add semvertoint

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// @flow
+const DEFAULT_BASE = 6
+
+const parseIntBase10 = (toParse) => parseInt(toParse, 10)
+
+const parseWithExponent = (base: number, exponent: number): number => {
+  return base * exponent + exponent
+}
+
+const semverToInt = (semver: string, base:number = 6) => {
+  const [mayor, minor, patch] = semver.split('.').map(parseIntBase10)
+  const baseInt = parseIntBase10(base)
+  const mayorInt = parseWithExponent(mayor, 10 ** baseInt)
+  const minorInt = parseWithExponent(minor, 10 ** (baseInt / 2))
+  
+  return mayorInt + minorInt + patch
+}
+
